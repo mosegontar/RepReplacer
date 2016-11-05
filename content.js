79a24237715f1f0ae5f67a11b7f6571c4fc2302f
Alex Gontar <mosegontar@gmail.com>
2016-11-05 15:52:24 -0400

    Replace found names with generic description

--- content.js
+++ content.js
@@ -14,23 +14,85 @@ bodyText = $('body').text().split(' ');
 
 function getMembers(lastname) {
     var members = data[lastname]
-    var firstnames = []
+    var names = {}
     for (var j = 0; j < members.length; j++) {
-        firstnames.push(members[j]['first_name']);
-    }
-    return firstnames;
+        if (typeof(members) === 'object') {
+            names[members[j]['first_name']] = [
+                members[j]['last_name'],
+                members[j]['state'],
+                members[j]['title']
+            ];
+        };
+    };
+    return names;
 }
+
+var foundNames = [];
 
 function parseResult(data) {
     for (var i = 0; i < bodyText.length; i++) {
         if (data[bodyText[i]] !== undefined) {
-            var firstNames = getMembers(bodyText[i]);
-            for (var n = 0; n < firstNames.length; n++) {
-                if (bodyText[i-1] === firstNames[n] || bodyText[i-2] === firstNames[n]) {
-                    console.log(firstNames[n]+' '+bodyText[i]);
-                }
+            var names = getMembers(bodyText[i]);
+            if (names[bodyText[i-1]] !== undefined) {
+                foundNames.push([
+                    bodyText[i], // lastname
+                    bodyText[i-1], // firstname,
+                    names[bodyText[i-1]][1], // state,
+                    names[bodyText[i-1]][2] // title
+                ]);
+            } else if (names[bodyText[i-2]] !== undefined) {
+                foundNames.push([
+                    bodyText[i], // lastname
+                    bodyText[i-2], // firstname,
+                    names[bodyText[i-2]][1], // state,
+                    names[bodyText[i-2]][2] // title
+                ]);
             }
+
         }
     }
+    if (foundNames.length > 1) {
+        console.log(foundNames);
+        walk(document.body);
+    }
+    
 }
+
+
+console.log
+
+function walk(node) {
+    // thanks to:
+    // http://stackoverflow.com/questions/5904914/javascript-regex-to-replace-text-not-in-html-attributes/5904945#5904945
+    // and
+    // 
+    var child, next;
+
+    switch (node.nodeType) {
+        case 1:
+        case 9:
+        case 11:
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+        case 3:
+            replaceText(node);
+            break;
+    }
+}
+
+function replaceText(textNode) {
+
+    for (var i = 0; i < foundNames.length; i++) {
+        var re = new RegExp(foundNames[i][0], "g");
+        textNode.nodeValue = textNode.nodeValue.replace(re, 'A '+foundNames[i][3].toLowerCase()+ ' from '+ foundNames[i][2]);
+    }
+
+
+}
+
 
