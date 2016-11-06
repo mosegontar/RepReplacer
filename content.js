24264059cb2e83b431c047322d65db65b4e8372b
Alex Gontar <mosegontar@gmail.com>
2016-11-06 01:57:04 -0500

    Add flexibility for variety of titles; DRY code

--- content.js
+++ content.js
@@ -59,13 +59,11 @@ function parseResult(data) {
 }
 
 
-console.log
-
 function walk(node) {
     // thanks to:
     // http://stackoverflow.com/questions/5904914/javascript-regex-to-replace-text-not-in-html-attributes/5904945#5904945
     // and
-    // 
+    // http://mattpic.com/talks/chrome-extensions
     var child, next;
 
     switch (node.nodeType) {
@@ -85,22 +83,56 @@ function walk(node) {
     }
 }
 
+
 function replaceText(textNode) {
 
-    textNode.nodeValue = textNode.nodeValue.replace(/Sen\./g, 'Senator');
-    textNode.nodeValue = textNode.nodeValue.replace(/Rep\./g, 'Representative');
 
-
-    for (var i = 0; i < foundNames.length; i++) {
-        var title = foundNames[i][3]; 
-        var re = new RegExp(title+' '+foundNames[i][1]+' '+ foundNames[i][0], "g");
-        textNode.nodeValue = textNode.nodeValue.replace(re, 'a '+title.toLowerCase()+ ' from '+ foundNames[i][2]);
-        textNode.nodeValue = textNode.nodeValue.replace(/^\S/g, function(x){return x.toUpperCase();});
-        textNode.nodeValue = textNode.nodeValue.replace(/\(D-.+\)/g, '');
-        textNode.nodeValue = textNode.nodeValue.replace(/\(R-.+\)/g, '');
+    function replaceInstance(rule) {
+        var re = RegExp(rule[0], "g")
+        textNode.nodeValue = textNode.nodeValue.replace(re, rule[1]);
     }
 
+    textNode.nodeValue = textNode.nodeValue.replace(/Sen\./g, 'Senator');
+    textNode.nodeValue = textNode.nodeValue.replace(/U\.S\. Senator/g, 'Senator');
+    textNode.nodeValue = textNode.nodeValue.replace(/Republican Senator/g, 'Senator');
+    textNode.nodeValue = textNode.nodeValue.replace(/Democratic Senator/g, 'Senator');        
+    textNode.nodeValue = textNode.nodeValue.replace(/Rep\./g, 'Representative');
+    textNode.nodeValue = textNode.nodeValue.replace(/\(D-.+\)/g, '');
+    textNode.nodeValue = textNode.nodeValue.replace(/\(R-.+\)/g, '');
+    textNode.nodeValue = textNode.nodeValue.replace(/\(D\)/g, '');
+    textNode.nodeValue = textNode.nodeValue.replace(/\(R\)/g, '');
+
+    for (var i = 0; i < foundNames.length; i++) {
+
+        var lastname = foundNames[i][0];
+        var firstname = foundNames[i][1];
+        var state = foundNames[i][2];
+        var titles = foundNames[i][3];
+
+        function getRules(title) {
+            var replacement = 'a '+title.toLowerCase()+ ' from ' +state
+            var rules = [
+                [state+' '+title+' '+firstname+' '+lastname, replacement],
+                ['Incumbent '+title+' '+firstname+' '+lastname, replacement],
+                [title+' '+firstname+' '+lastname, replacement],
+                ['Republican '+firstname+' '+lastname, replacement],
+                ['Democrat '+firstname+' '+lastname, replacement],
+                ['Republican '+title+' '+firstname+' '+lastname, replacement],
+                ['Democratic '+title+' '+firstname+' '+lastname, replacement],                
+                [firstname+' '+lastname, replacement],
+                [lastname, 'the '+state+' '+title.toLowerCase()],
+            ] 
+            return rules;           
+        }
+
+        for (var t = 0; t < titles.length; t++) {
+            var rules = getRules(titles[t]);
+            for (var r = 0; r < rules.length; r ++) {
+                replaceInstance(rules[r]);
+            };
+        }
+    }
+
+    textNode.nodeValue = textNode.nodeValue.replace(/^\S/g, function(x){return x.toUpperCase();});
 
 }
-
-
